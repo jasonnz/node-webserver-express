b25738d7f51bcf6bc5daa772fb83aecd592163aa
Jason Marshall <jasonnz@gmail.com>
2019-12-08 22:17:33 +1300

    Added in handle bar partials to views

--- src/app.js
+++ src/app.js
@@ -5,19 +5,24 @@ const hbs = require('hbs');
 const app = express();
 
 // Define paths for express config
-const viewsPath = path.join(__dirname,'../templates');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to server
 app.use(express.static(path.join(__dirname, '../public')));
 
+// App get for route handling
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather',
-    name: 'Jason'
+    name: 'Jason Marshall'
+
   });
 });
 
@@ -30,14 +35,31 @@ app.get('/about', (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    message: "This is a help message"
+    message: "This is a help message",
+    title: 'Help Title',
+    name: 'Jason Marshall'
   });
 });
 
 app.get("/weather", (req, res) => {
   res.send({
       location: "Wellington",
-      forecast: "Pleasant evening 15 degress"
+      forecast: "Pleasant evening 15 degress",
+      name: 'Jason Marshall'    
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', { 
+    message: "404 - Your HELP article not found!",
+    name: 'Jason Marshall' 
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', { 
+    message: "404 - Your page cannot be found!",
+    name: 'Jason Marshall' 
   });
 });
 
