42df67be8796d0aa6510b31894ad1e55a7567cdf
Jason Marshall <jasonnz@gmail.com>
2019-12-17 22:26:38 +1300

    added more stylings and weather displaying on the index page

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,33 @@
 console.log('Client sode Javascript file is loaded');
 
 
-fetch('http://localhost:3000/weather?address=Wellington').then((res) => {
-    res.json().then((data) => {
-        
-        if (data.error) {
-            console.log('An Error Occured');
-        } else {
-            console.log(data);
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+
+    if (!location) return console.log('Location must not be ' + location);  
+
+    messageOne.textContent = 'Loading'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((res) => {
+        res.json().then((data) => {
+
+            if (data.error) {
+                const errorMsg = 'An error has occured, try another location'
+                messageOne.textContent = errorMsg
+                console.log(errorMsg);
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+                console.log(data);
+            }
+        })
     })
+
 })
