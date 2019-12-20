1b0f5e43143815f12321ad360ed31080be16bb95
Jason Marshall <jasonnz@gmail.com>
2019-12-21 10:28:23 +1300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
-const path =require('path');
-const express = require('express');
-const hbs = require('hbs');
-const geocode = require("./utils/geocode");
-const forecast = require("./utils/forecast");
+const path =require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require("./utils/geocode")
+const forecast = require("./utils/forecast")
 
-const app = express();
+const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
-const viewsPath = path.join(__dirname,'../templates/views');
-const partialsPath = path.join(__dirname, '../templates/partials');
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
-hbs.registerPartials(partialsPath);
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to server
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')))
 
 // App get for route handling
 app.get('', (req, res) => {
@@ -25,23 +26,23 @@ app.get('', (req, res) => {
     title: 'Weather',
     name: 'Jason Marshall'
 
-  });
-});
+  })
+})
 
 app.get('/about', (req, res) => {
   res.render("about", {
     title: "About me",
     name: "Jason Marshall"
-  });
-});
+  })
+})
 
 app.get("/help", (req, res) => {
   res.render("help", {
     message: "This is a help message",
     title: 'Help Title',
     name: 'Jason Marshall'
-  });
-});
+  })
+})
 
 app.get("/weather", (req, res) => {
   
@@ -52,36 +53,36 @@ app.get("/weather", (req, res) => {
   }
 
   geocode(req.query.address, (error, { lattitude, longtitude, location } = {}) => {
-    if (error) return console.log(error);
+    if (error) return console.log(error)
 
     forecast(lattitude, longtitude, (error, { forecastdata }) => {
-      if (error) return console.log(error);
-      console.log(forecastdata);
+      if (error) return console.log(error)
+      console.log(forecastdata)
 
       res.send({
         forecast: forecastdata,
         location: location,
         address: req.query.address
-      });
+      })
       
-    });
-  });
-});
+    })
+  })
+})
 
 app.get('/help/*', (req, res) => {
   res.render('404', { 
     message: "404 - Your HELP article not found!",
     name: 'Jason Marshall' 
-  });
-});
+  })
+})
 
 app.get('*', (req, res) => {
   res.render('404', { 
     message: "404 - Your page cannot be found!",
     name: 'Jason Marshall' 
-  });
-});
+  })
+})
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+app.listen(port, () => {
+    console.log('Server started on port ', port)
+})
